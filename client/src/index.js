7318c383a28fd9946bba2e7f79c6de29f1c0f068
vangarasrinivas <47146476+vangarasrinivas@users.noreply.github.com>
2021-08-05 11:11:45 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import AppUserJWT from "./User Auth JWT Client/App";
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 import reportWebVitals from "./reportWebVitals";
 
